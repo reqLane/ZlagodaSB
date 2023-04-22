b3fe3c053463e0ddd209ed2bd3c8903ff45dc22e
reqLane <vlad5000191@gmail.com>
2023-04-23 00:36:02 +0300

    Initial commit

---
+++ src/main/java/com/example/zlagodasb/ZlagodaSbApplication.java
@@ -0,0 +1,13 @@
+package com.example.zlagodasb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZlagodaSbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZlagodaSbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/zlagodasb/ZlagodaSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zlagodasb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZlagodaSbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
